640eda7a0ed14c5d2da1a13ac1cba1f21ef35fa5
beatrixyu <beatrixyu@126.com>
2021-04-16 21:50:59 +0200

    renew

--- main.js
+++ main.js
@@ -1,31 +1,53 @@
-
-
-
-// ===== Scroll to bottom ==== 
+// Scroll to the text 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#click-to-bottom').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#click-to-bottom').fadeOut(200);   // Else fade out the arrow
-    }
-  });
-  $('#click-to-bottom').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-  });
+  if ($(this).scrollTop() >= 1) {        
+      $('#click-to-bottom').fadeIn(0);    
+  }
+});
+$('#click-to-bottom').click(function() {      
+  $('body,html').animate({
+      scrollTop : 1000                      
+  }, 500);
+});
 
-
-// ===== Scroll to Top ==== 
+// Scroll to Top 
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-  });
-  $('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-  });
+  if ($(this).scrollTop() >= 50) {        
+      $('#return-to-top').fadeIn(200);    
+  } else {
+      $('#return-to-top').fadeOut(200);   
+  }
+});
+$('#return-to-top').click(function() {     
+  $('body,html').animate({
+      scrollTop : 0                      
+  }, 500);
+});
+
+// Slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length};
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+  dots[i].classList.remove("active");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].classList.add("active");
+}
